1fe022b582955d56bee0081c908717dea7cfca08
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-06 19:56:01 -0300

    eurekaconfor

--- src/eureka.ts
+++ src/eureka.ts
@@ -4,27 +4,3 @@ if (clave === "eureka") {
 } else {
   console.log("clave incorrecta");
 }
-/* 
-// con for
-let codigo: string = prompt("Ingrese la clave: ");
-
-let intento: number = 0;
-let intentorest: number = 2;
-
-
-for (intento = 0; intento < 2; intento++) {
-  if (codigo === "Eureka") {
-    console.log("Clave correcta, ingresando al sistema");
-    intento = 2;
-  } else {
-    codigo = prompt(
-      "La clave es incorrecta, te quedan " +
-        intentorest +
-        " intentos, Ingrese la clave nuevamente : "
-    );
-  }
-  intentorest -= 1;
-}
-if (codigo !== "Eureka") {
-  console.log("Clave incorrecta 
-  */
