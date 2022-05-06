de0d3e78cdac14f0c8ee2ab9ad25e88c001745eb
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-06 20:13:14 -0300

    multiplosconfor

--- src/multiplos.ts
+++ src/multiplos.ts
@@ -1,4 +1,4 @@
-let numero: number = Number(prompt("ingrese un numero"));
+/* let numero: number = Number(prompt("ingrese un numero"));
 if ((numero & 2) === 0) {
   console.log("numero es multiplo de 2");
 } else {
@@ -8,4 +8,9 @@ if (numero % 3 === 0) {
   console.log("numero es multiplo de 3");
 } else {
   console.log("numero no es multiplo de 3");
+}
+*/
+
+for (let indice: number = 1; indice < 100; indice++) {
+  console.log(indice);
 }
