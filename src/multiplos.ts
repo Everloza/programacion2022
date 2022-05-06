6d7cbea55a3d12dda45990f0c92b4109806b911a
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-06 20:23:18 -0300

    multiploconbinadoconFor

--- src/multiplos.ts
+++ src/multiplos.ts
@@ -1,16 +1,5 @@
-/* let numero: number = Number(prompt("ingrese un numero"));
-if ((numero & 2) === 0) {
-  console.log("numero es multiplo de 2");
-} else {
-  console.log("numero no es multiplo de 2");
-}
-if (numero % 3 === 0) {
-  console.log("numero es multiplo de 3");
-} else {
-  console.log("numero no es multiplo de 3");
-}
-*/
-
 for (let indice: number = 1; indice < 100; indice++) {
-  console.log(indice);
+  if ((indice & 2) === 0 || indice % 3 === 0) {
+    console.log(indice);
+  }
 }
