8af266a934b9149ccf02c57a21fb8421ccf8b2f7
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-06 21:53:15 -0300

    encontrarelmaximo

---
+++ src/parOImpar.ts
@@ -0,0 +1,10 @@
+let parImpar: number = Number(prompt("ingresar nunmero"));
+let numero: number = 0;
+
+if (numero % 2 === 0) {
+  console.log("es numero par", par);
+} else if (10 % 3 === 1) {
+  console.log("es numero impar", impar);
+} else {
+  console.log("ingresar numero nuevamente");
+}
--- src/sumaDeNumeros.ts
+++ src/sumaDeNumeros.ts
@@ -1,4 +1,19 @@
-let numero1: number = Number(prompt("ingrese primer numero"));
+let numero1: number = Number(prompt("ingresa numero 1"));
+let numero2: number = Number(prompt("ingresa numero 2"));
+let suma: number = 0;
+
+// intercambio de variables IMPORTANTE.
+if (numero1 > numero2) {
+  let aux: number = numero1; //recomendable usar el AUXiliar dentro del if. auxiliar igual numero 5
+  numero1 = numero2;
+  numero2 = aux;
+}
+for (let indice: number = numero1; indice <= numero2; indice++) {
+  suma = suma + indice;
+}
+console.log(suma);
+
+/*let numero1: number = Number(prompt("ingrese primer numero"));
 let numero2: number = Number(prompt("ingrese segundo numero"));
 let suma: number = 0;
 if (numero1 <= numero2) {
@@ -12,3 +27,4 @@ if (numero1 <= numero2) {
 }
 
 console.log("su resultado es", suma);
+*/
