6fad1f53f6afaecb447fda0109f40ed2d9b16cda
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-10 18:29:10 -0300

    tarea9abril

--- src/Tarea9deAbril.ts
+++ src/Tarea9deAbril.ts
@@ -1,20 +1,20 @@
-let numero: number = 1;
-let positivo: number = 99999;
-let negativo: number = -99999;
-let contador: number = 0;
-let promedio: number = 0;
+let numero: number = 0;
+let positivo: number = 0;
+let porcentaje: number = 0;
 
 while (numero !== 0) {
-  numero = Number(prompt("ingrese un numero distinto a 0: "));
-  console.log("el numero es: " + numero);
-  if (numero < positivo) {
-    positivo = numero;
-  }
-  if (numero > negativo) {
-    negativo = numero;
-  }
-  contador++;
-  promedio = (numero * 100) / promedio;
-}
+  //mientras que el numero sea distinto a 0
+  numero = Number(prompt("ingrese un numero distinto a 0: ")); //ingreso el numero
 
-console.log("el porcentaje de positivos es: " + promedio);
+  if (numero > 0) {
+    //pregunto si el numero es positivo
+    positivo++; //cuenta la cantidad de numeros positivos
+  }
+  numero++; //cuenta la cantidad de numeros en total
+  numero = Number(prompt("ingresar un numero: ")); //ingresar nuevo numero
+  if (porcentaje > 0) {
+    //pregunto si el porcentaje es mayor a 0
+    porcentaje = (positivo / numero) * 100; //calculo el porcentaje
+    console.log("el porcentaje de positivos es: ", porcentaje);
+  }
+}
