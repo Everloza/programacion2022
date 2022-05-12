b820a545b9c7175265d0f9a4f332e2225fb3b5eb
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-12 20:42:42 -0300

    tarea12deabril6

--- src/Tarea12deAbril.ts
+++ src/Tarea12deAbril.ts
@@ -13,28 +13,28 @@ function DibujarLineas() {
     //utilizo un contador para las lineas (hasta 40 lineas)
     linea = linea + "-"; //sumo la variable linea con el string
   }
-  console.log(linea);
+  console.log(linea); //imprimo la linea
 }
 
 switch (opcion) {
   case 1: //suma
-    DibujarLineas();
-    console.log("el resultado es: ", numero1 + numero2);
+    DibujarLineas(); //funcion linea
+    console.log("el resultado es: ", numero1 + numero2); //imprimo suma
     DibujarLineas();
     break;
   case 2: //resta
     DibujarLineas();
-    console.log("el resultado es: ", numero1 - numero2);
+    console.log("el resultado es: ", numero1 - numero2); //imprimo resta
     DibujarLineas();
     break;
   case 3: //multiplicacion
     DibujarLineas();
-    console.log("el resultado es: ", numero1 * numero2);
+    console.log("el resultado es: ", numero1 * numero2); //imprimo multiplicacion
     DibujarLineas();
     break;
   case 4: //division
     DibujarLineas();
-    console.log("el resultado es: ", numero1 / numero2);
+    console.log("el resultado es: ", numero1 / numero2); //imprimo division
     DibujarLineas();
     break;
   default: //cierre
