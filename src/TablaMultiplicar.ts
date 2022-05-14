9998f786cc0d7cf223a4733336388157cc2227bd
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-14 11:47:39 -0300

    tablademultiplicar2

--- src/TablaMultiplicar.ts
+++ src/TablaMultiplicar.ts
@@ -1,10 +1,12 @@
-let numero: number = Number(prompt("ingrese numero: "));
+let numero: number = Number(prompt("ingrese numero a multiplicar: "));
 //declaro -numero- para ingresar a la consola.
-let tope: number = Number(prompt("ingrese tope: "));
-//declaro el -tope-, que seria el limite para cada operacion.
+let tope: number = Number(prompt("ingrese el tope para el multiplo: "));
+//declaro -tope-, que seria un limite para cada multipliacion.
 
 for (let indice: number = 0; indice <= tope; indice++) {
-  //el for para repeticion indica, indice igual a 0, menos igual al limite y el contador
+  //el for para repeticion declara la variable -indice- de tipo numero igual a 0,
+  // -indice- menor igual a la variable tope,
+  // y un contador ++ para -indice-.
   console.log(numero + " x " + indice + " = " + numero * indice);
   //imprime en la consola la operacion,la variable -numero- suma el string "x(por)",
   // suma la variable -indice- que es menor igual al -tope- (limite),
