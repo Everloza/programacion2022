807f71318f3f70af50e813e61b72ee30e6770436
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-14 12:04:09 -0300

    tarea12DeAbrilMasComentarios

--- src/Tarea12deAbril.ts
+++ src/Tarea12deAbril.ts
@@ -1,40 +1,41 @@
-let numero1: number = Number(prompt("Ingrese primer numero: ")); //declaro primer valor
-let numero2: number = Number(prompt("Ingrese segundo numero: ")); //declaro segundo valor
+let numero1: number = Number(prompt("Ingrese primer numero: ")); //declaro primer valor.
+let numero2: number = Number(prompt("Ingrese segundo numero: ")); //declaro segundo valor.
 let opcion: number = Number(
   prompt(
     "ingrese 1 para sumar, 2 para restar, 3 para multiplicar, 4 para dividir: "
   )
-); //declaro las opciones de operacion
+); //declaro las opciones de operacion.
 
 function dibujarLineas() {
-  //declaro la funcion dibujarlineas, que luego las utilizo en cada operacion
-  let linea = "-"; //declaro linea en string
+  //declaro la funcion dibujarlineas, que luego las utilizo en cada operacion.
+  let linea = "-"; //declaro linea en string.
   for (let i: number = 0; i <= 40; i++) {
-    //utilizo un contador para las lineas (hasta 40 lineas)
-    linea = linea + "-"; //sumo la variable linea con el string
+    //utilizo un contador para las lineas (hasta 40 lineas).
+    linea = linea + "-"; //sumo la variable linea con el string.
   }
-  console.log(linea); //imprimo la linea
+  console.log(linea); //imprimo la linea.
 }
 
+//declaro -opcion- dentro del switch.
 switch (opcion) {
-  case 1: //suma
-    dibujarLineas(); //funcion linea
-    console.log("el resultado es: ", numero1 + numero2); //imprimo suma
+  case 1: //primero case para la suma.
+    dibujarLineas(); //funcion de la linea implementada dentro de cada case.
+    console.log("el resultado es: ", numero1 + numero2); //imprimo el resultado de la suma de las dos variables.
+    dibujarLineas();
+    break; //cierro el case.
+  case 2: //segundo case para la resta.
+    dibujarLineas();
+    console.log("el resultado es: ", numero1 - numero2); //imprimo el resultado de la resta de las dos variables.
     dibujarLineas();
     break;
-  case 2: //resta
-    dibujarLineas();
-    console.log("el resultado es: ", numero1 - numero2); //imprimo resta
-    dibujarLineas();
-    break;
-  case 3: //multiplicacion
-    dibujarLineas();
-    console.log("el resultado es: ", numero1 * numero2); //imprimo multiplicacion
+  case 3: //tercer caso para la multiplicacion.
+    dibujarLineas(); //funcion de la linea implementada dentro de cada case.
+    console.log("el resultado es: ", numero1 * numero2); //imprimo el resultado de la mult. de las dos variables.
     dibujarLineas();
     break;
-  case 4: //division
+  case 4: //cuarto case para la division.
     dibujarLineas();
-    console.log("el resultado es: ", numero1 / numero2); //imprimo division
+    console.log("el resultado es: ", numero1 / numero2); //imprimo el resultado de la div. de las dos variables.
     dibujarLineas();
     break;
   default: //cierre
