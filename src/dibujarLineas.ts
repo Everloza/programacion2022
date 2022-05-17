a6668785e8b8c203f15c89e7136401bc488c34b7
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-17 11:08:52 -0300

    funcionconparametroretorn

---
+++ src/dibujarLineas.ts
@@ -0,0 +1,12 @@
+function dibujarLinea(numero: Number): string {
+  let linea: string = "";
+  for (let indice: number = 0; indice < numero; indice++) {
+    linea = linea + "-";
+  }
+  return linea;
+}
+
+for (let indice: number = 1; indice < 10; indice++) {
+  let linea: string = dibujarLinea(indice);
+  console.log(linea);
+}
