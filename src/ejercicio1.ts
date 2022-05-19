6275ddc0619c74768ab162607e065daa47ad9aad
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-19 12:16:15 -0300

    ejercicio1

--- src/ejercicio1.ts
+++ src/ejercicio1.ts
@@ -1,7 +1,48 @@
-console.log(5 * 10);
+// Crear un archivo nuevo que se denomine ejercicio1.ts e
+// imprimir por consola los pasos para:
+// a) Cocinar una torta
+// b) Dirigirse desde su domicilio hasta el supermercado
+// c) Crear un posteo en facebook
+// d) O cualquier otro procedimiento que el alumno desee
+// (Mínimo 10 instrucciones)
+
+console.log("-----------------------");
+console.log("¿Como Cocinar una torta?");
+console.log("-----------------------");
+console.log(
+  "Paso 1: Comenzar con la mantequilla y el azúcar. Mezcla ambas y bátelas hasta conseguir una crema color blanquecino."
+);
+console.log(
+  "Paso 2: La forma correcta de mezclar estos ingredientes es con la mantequilla blanda, para obtenerla debes sacar la mantequilla de la nevera al menos una media hora antes de la preparación para asegurar que esté a temperatura ambiente."
+);
+console.log(
+  "Paso 3: Mientras comienzas la receta, puedes ir encendiendo el horno para que, al momento de introducir la mezcla de tu ponqué de vainilla, este ya esté precalentado. Puedes precalentar a 150º C y así te asegurarás de que tu bizcocho casero se horneará uniformemente.."
+);
+console.log(
+  "Paso 4: Incorpora poco a poco el resto de los ingredientes para la torta. Primero, añade los huevos y mezcla. "
+);
+console.log("Paso 5: Agrega la harina y vuelve a mezclar.");
+console.log("paso 6: Integra la leche hasta obtener una mezcla homogénea.");
+console.log(
+  "paso 7: Agrega los toques de vainilla, ron o whisky y la ralladura de limón. Si prefieres no usar licor, no hay problema."
+);
+console.log(
+  "paso 8: El sabor predominante de esta torta casera debe ser la vainilla, pero puedes modificarlo al gusto por cualquier otra esencia. La ralladura de naranja también le queda muy bien a este bizcocho esponjoso."
+);
+console.log(
+  "paso 9: Ahora, en un molde previamente untado de mantequilla, coloca la mezcla y llévala al horno durante 40 minutos, a 180 ºC."
+);
+console.log(
+  "paso 10:Para terminar la torta clásica de vainilla solo te queda sacarla del horno y esperar a que enfríe a temperatura ambiente para desmoldarla. Este bizcocho casero podrás disfrutarlo así solo, sin más. Si te animas con la repostería, puedes decorarlo espolvoreando el tope con azúcar glass, hacer un relleno con frosting de chocolate o darle un motivo especial con fondant de azúcar."
+);
+console.log("Fin");
+
+/* console.log(5 * 10);
 
 let numero_1: number = 3;
 let numero_2: number = 2;
 
 let resultado: number = numero_1 + numero_2;
 console.log(resultado);
+
+*/
