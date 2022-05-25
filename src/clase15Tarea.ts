c501171037f91f5fae22ae43793faca1ec7d5dd6
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-25 18:17:16 -0300

    actualizacion25mayo

--- src/clase15Tarea.ts
+++ src/clase15Tarea.ts
@@ -1,3 +1,30 @@
+/* Clase 15 - Modularizacion y funciones 2era Parte - Métodos - Ejercicio: Divisores.
+
+Visto
+
+-Métodos (repaso)
+-Métodos con parámetros
+-Métodos con retornos (return)
+-Modularizarían 
+
+Trabajado
+
+-Suma entre números modularizando
+-Suma entre números con parámetros
+-Suma entre números con retorno
+-Sacar el área de un triangulo
+-Calcular la potencia de un numero
+-Sacar mediante un método que retorna un booleano si un numero es o no múltiplo de otro
+
+Actividad
+
+    Implemente un método llamado cantidadDeDivisores que reciba un número entero y devuelva la cantidad de divisores 
+    Por ejemplo, para el número 16, sus divisores son 1, 2, 4, 8, 16, por lo que la respuesta debería ser 5 
+
+           Re-utilice el método esMultiplo implementado para el ejercicio anterior.
+
+*/
+
 function esMultiplo(dividendo: number, divisor: number): boolean {
   if (dividendo % divisor === 0) {
     return true;
---
+++ src/clase17Tarea.ts
@@ -0,0 +1,27 @@
+/* Clase 17 - Legibilidad y Arreglos - 
+
+Visto
+
+-Modularizarían (repaso)
+-Estructuras de datos
+-Arreglos
+
+Trabajado
+
+-Crear Arreglos
+-Asignar datos
+-Recorrer Arreglos
+-Mostrar Arreglos
+- Función Math.random()
+
+Actividad (Sumar dos Arreglos)
+
+    Sumar los elementos de cada una de las posiciones de dos arreglos y guardar el resultado en otro arreglo.
+    El arreglo tiene dimensión 6 y los números de los dos vectores los carga el usuario.
+
+Ejemplo:
+v1 =        1, 3, 7, 9, 9, 5 
+v2 =        6, 9, 2, 5, 9, 4vSuma =    7, 12, 9, 14, 18, 9
+
+Nro Filimina: 54.
+*/
