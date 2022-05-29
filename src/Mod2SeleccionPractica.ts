7c0570358de5526dfad02520f465d401ad428cae
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-29 08:59:56 -0300

    mod2practiica2

--- src/Mod2SeleccionPractica.ts
+++ src/Mod2SeleccionPractica.ts
@@ -1,10 +1,13 @@
 /*
 console.log(4 <= 2);
 */
+
+// Operadores Relacionales
 let numero1: number = 3;
 let numero2: number = 5;
 let resultado: boolean;
 
 resultado = numero1 < numero2; // resultado = true
-resultado = n;
+resultado = numero1 > numero2; //resultado = false
+
 console.log(resultado);
