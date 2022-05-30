f4ec0c162e66951e726e322f0a2df2dc32026a6b
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-05-29 21:57:39 -0300

    seleccion3

---
+++ src/ejercicioSeleccionSimple2.ts
@@ -0,0 +1,18 @@
+/* Ejercicio – Mayor de Tres
+• Desarrolle un algoritmo que dados tres
+números determine cuál es el mayor de los
+tres. 
+ */
+let numero1: number = Number(prompt("Ingrese un 1er numero:"));
+let numero2: number = Number(prompt("Ingrese un 2do numero:"));
+let numero3: number = Number(prompt("Ingrese un 3er numero:"));
+
+if (numero1 > numero2 && numero1 > numero3) {
+  console.log(numero1 + " es mayor a " + numero2 + " y a " + numero3);
+} else if (numero2 > numero3 && numero2 > numero1) {
+  console.log(numero2 + " es mayor a " + numero3 + " y a " + numero1);
+} else if (numero3 > numero2 && numero3 > numero1) {
+  console.log(numero3 + " es mayor a " + numero2 + " y a " + numero1);
+} else {
+  console.log("Ingrese nuevamente");
+}
---
+++ src/ejercicioSeleccionSimple3.ts
@@ -0,0 +1,29 @@
+/*  Ejercicio – Descuento Octubre
+
+• Una tienda al cumplir años en Octubre ofrece
+un descuento del 15% a sus clientes en todas
+sus compras.
+• Desarrolle un algoritmo que dada una compra:
+precio unitario, cantidad y el mes, indicados
+por el usuario, determine si el cliente tiene
+descuento o no.
+*/
+
+let precioUnitario: number = Number(prompt("Ingrese el precio: "));
+let cantidad: number = Number(prompt("Ingrese la cantidad: "));
+let mes: string = String(prompt("Ingresar mes"));
+
+let descuento15: number;
+let totalDescuento15: number;
+let totalSinDescuento: number = precioUnitario * cantidad;
+
+if (mes === "octubre") {
+  descuento15 = (precioUnitario * 15) / 100;
+  totalDescuento15 = totalSinDescuento - descuento15;
+  console.log("Por comprar en el mes de Octubre Ud tiene un 15% de descuento");
+  console.log("El monto a pagar es: " + totalDescuento15);
+} else {
+  console.log(
+    "Ud. no tiene descuento, el monto a pagar es: " + totalSinDescuento
+  );
+}
