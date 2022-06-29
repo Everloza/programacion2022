88b7dca86caf74a93898f55627fcca3275338654
Ever Loza <104173490+Everloza@users.noreply.github.com>
2022-06-28 22:54:24 -0300

    ijiji

---
+++ src/tpRec.ts
@@ -0,0 +1,21 @@
+/* 
+Hacer un programa que simule la compra que hace un cliente en un
+supermercado. El cliente va tener que ingresar por teclado la
+CANTIDAD de productos que va a llevar, los PRODUCTOS que va
+a comprar, el PRECIO UNITARIO de cada producto y la CANTIDAD
+A COMPRAR DE CADA producto.
+Finalizada la compra, el programa va a mostrar por pantalla el
+detalle de la compra, productos, cantidad, precio unitario y precio
+total.
+Dependiendo del monto comprado, el cliente participa de un sorteo:
+• si compra menos de 1000 no participa por nada
+• si compra mas de 3000 participa por un 0km
+• si compra mas de 2000 pero menos de 3000 participa por una
+moto 0k
+• si compra mas de 1000 pero menos de 2000 participa un tv led
+
+*/
+
+function (){
+  
+}
